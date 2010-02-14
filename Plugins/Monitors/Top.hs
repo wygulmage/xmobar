75e97337a0f68caacbfa7ddadcf040c6f3f889df
Jose A Ortega Ruiz <jao@gnu.org>
2010-02-14 14:42:35 +0100

    A few redundant bang patterns removed
    Ignore-this: aaac0bc989a6c19c548c9917739bdf46
    
    darcs-hash:20100214134235-748be-0ee80de1e2f175aee2b8252ac9fc8cc3bd78a199.gz

--- Plugins/Monitors/Top.hs
+++ Plugins/Monitors/Top.hs
@@ -102,8 +102,8 @@ type Meminfo = (String, Int)
 
 meminfo :: [String] -> Meminfo
 meminfo fs = (n, r)
-  where !n = processName fs
-        !r = pageSize * (read (fs!!23))
+  where n = processName fs
+        r = pageSize * (read (fs!!23))
 
 meminfos :: IO [Meminfo]
 meminfos = handleProcesses ("", 0) meminfo
@@ -132,9 +132,9 @@ type TimesRef = IORef Times
 
 timeEntry :: [String] -> TimeEntry
 timeEntry fs = (p, (n, t))
-  where !p = read (head fs)
-        !n = processName fs
-        !t = read (fs!!13) + read (fs!!14)
+  where p = read (head fs)
+        n = processName fs
+        t = read (fs!!13) + read (fs!!14)
 
 timeEntries :: IO [TimeEntry]
 timeEntries = handleProcesses (0, ("", 0)) timeEntry
@@ -168,7 +168,7 @@ runTopCpu tref lapse _ = do
 
 startTopCpu :: [String] -> Int -> (String -> IO ()) -> IO ()
 startTopCpu a r cb = do
-  t <- getSysVar ClockTick
+  cr <- getSysVar ClockTick
   tref <- newIORef M.empty
-  let lapse = (fromIntegral r * fromIntegral t) / 10
+  let lapse = (fromIntegral r * fromIntegral cr) / 10
   runM a topCpuConfig (runTopCpu tref lapse) r cb
