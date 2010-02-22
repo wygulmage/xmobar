fd7de94c92b5d094bd244a06f8b330237ed7e99d
Jose A Ortega Ruiz <jao@gnu.org>
2010-02-22 10:59:13 +0100

    Getting the padding right
    Ignore-this: 6bf3b6c26ba135f473a355a480fb7efa
    
    darcs-hash:20100222095913-748be-b44866d31bd677f28798cbe5c124e63a564c273a.gz

--- Plugins/Monitors/Wireless.hs
+++ Plugins/Monitors/Wireless.hs
@@ -26,6 +26,8 @@ runWireless (iface:_) = do
   let essid = wiEssid wi
       quality = wiQuality wi
       e = if essid == "" then "N/A" else essid
-  q <- if quality >= 0 then showWithColors show quality else return " "
+  q <- if quality >= 0
+       then showWithColors show quality
+       else showWithColors' "" (0 :: Int)
   parseTemplate [e, q]
 runWireless _ = return ""
