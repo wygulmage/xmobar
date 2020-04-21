0cfa5f8daa6ff6b7f2322cc035ac82f3b5d7f279
jao <jao@gnu.org>
2020-04-21 04:24:12 +0100

    Whitespace

--- src/Xmobar/Plugins/Monitors/MPD.hs
+++ src/Xmobar/Plugins/Monitors/MPD.hs
@@ -101,13 +101,15 @@ parseMPD (Right st) song opts = do
   bar <- showPercentBar (100 * b) b
   vbar <- showVerticalBar (100 * b) b
   ipat <- showIconPattern (mLapsedIconPattern opts) b
-  return $ [bar, vbar, ipat, ss, si, vol, len, lap, remain, plen, ppos, flags] ++ songData
+  return $ [bar, vbar, ipat, ss, si, vol, len, lap, remain, plen, ppos, flags]
+           ++ songData
   where s = M.stState st
         ss = show s
         si = stateGlyph s opts
         vol = int2str $ fromMaybe 0 (M.stVolume st)
         (p, t) = fromMaybe (0, 0) (M.stTime st)
-        [lap, len, remain] = map showTime [floor p, floor t, max 0 (floor t - floor p)]
+        [lap, len, remain] = map showTime
+                                 [floor p, floor t, max 0 (floor t - floor p)]
         b = if t > 0 then realToFrac $ p / t else 0
         plen = int2str $ M.stPlaylistLength st
         ppos = maybe "" (int2str . (+1)) $ M.stSongPos st
